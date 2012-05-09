33acb06b78a4f7cd7a8f8cc6e3eac4e1049a682b
Christophe Gueret <christophe.gueret@gmail.com>
2012-05-09 12:41:45 +0200

    Minor fixes

--- src/main/java/nl/vu/datalayer/hbase/HBaseConnection.java
+++ src/main/java/nl/vu/datalayer/hbase/HBaseConnection.java
@@ -7,32 +7,32 @@ import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.client.HBaseAdmin;
 
 public class HBaseConnection {
-	
+
 	protected HBaseAdmin hbase = null;
 	protected Configuration conf = null;
-	
+
 	public static final String ZOOKEEPER_QUORUM = "das3001.cm.cluster,das3002.cm.cluster,das3003.cm.cluster,das3004.cm.cluster,das3005.cm.cluster";
 
 	public HBaseConnection() throws Exception {
 		conf = HBaseConfiguration.create();
-	    
-		//conf.set("hbase.master","fs0.cm.cluster:60000");
+
+		// conf.set("hbase.master","fs0.cm.cluster:60000");
+		conf.set("hbase.master", "fs0.das4.cs.vu.nl:60010");
 		conf.set("hbase.zookeeper.quorum", ZOOKEEPER_QUORUM);
-		
+
 		System.out.println(conf);
 		hbase = new HBaseAdmin(conf);
 	}
-	
+
 	public HBaseAdmin getAdmin() {
 		return hbase;
 	}
 
-	
 	public Configuration getConfiguration() {
 		return conf;
 	}
-	
-	public void close() throws IOException{
+
+	public void close() throws IOException {
 		hbase.close();
 	}
 
