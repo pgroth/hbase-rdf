7f33139f500e411cfef48bf054d00cc16334d3d4
fundatureanu-sever <fundatureanu.sever@gmail.com>
2012-05-31 17:38:17 +0200

    Added another parameter for the type of HBaseConnection: native-java or REST

--- quad_retrieve.sh
+++ quad_retrieve.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mvn exec:java -Dexec.mainClass="nl.vu.datalayer.hbase.RetrieveQuads" -Dexec.args="quad_queries" 
+mvn exec:java -Dexec.mainClass="nl.vu.datalayer.hbase.RetrieveQuads" -Dexec.args="quad_queries native-java" 
 
