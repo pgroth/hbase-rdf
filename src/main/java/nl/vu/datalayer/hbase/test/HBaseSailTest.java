a3621db13aa92091a16288d9fc4aa511cddcbc0a
Christophe Gueret <christophe.gueret@gmail.com>
2012-07-10 16:18:11 +0200

    .

--- src/main/java/nl/vu/datalayer/hbase/test/HBaseSailTest.java
+++ src/main/java/nl/vu/datalayer/hbase/test/HBaseSailTest.java
@@ -1,5 +1,9 @@
 package nl.vu.datalayer.hbase.test;
 
+import nl.vu.datalayer.hbase.sail.HBaseRepositoryConnection;
+import nl.vu.datalayer.hbase.sail.HBaseSail;
+import nl.vu.datalayer.hbase.sail.HBaseSailRepository;
+
 import org.openrdf.model.Value;
 import org.openrdf.query.BindingSet;
 import org.openrdf.query.MalformedQueryException;
@@ -7,21 +11,15 @@ import org.openrdf.query.QueryEvaluationException;
 import org.openrdf.query.QueryLanguage;
 import org.openrdf.query.TupleQuery;
 import org.openrdf.query.TupleQueryResult;
-import org.openrdf.repository.Repository;
-import org.openrdf.repository.RepositoryConnection;
 import org.openrdf.repository.RepositoryException;
 import org.openrdf.sail.SailException;
-
-import nl.vu.datalayer.hbase.sail.HBaseRepositoryConnection;
-import nl.vu.datalayer.hbase.sail.HBaseSail;
-import nl.vu.datalayer.hbase.sail.HBaseSailRepository;
 
 public class HBaseSailTest {
 
 	/**
 	 * @param args
-	 * @throws SailException 
-	 * @throws RepositoryException 
+	 * @throws SailException
+	 * @throws RepositoryException
 	 */
 	public static void main(String[] args) throws SailException, RepositoryException {
 		// TODO Auto-generated method stub
@@ -29,24 +27,24 @@ public class HBaseSailTest {
 		mySail.initialize();
 		HBaseSailRepository myRepo = new HBaseSailRepository(mySail);
 		HBaseRepositoryConnection conn = myRepo.getConnection();
-//		conn
-	
+		// conn
+
 		String queryString = "SELECT ?g FROM NAMED <http://en.wikipedia.org/wiki/Alabama#>  WHERE { GRAPH ?g { <http://dbpedia.org/resource/Alabama> <http://dbpedia.org/ontology/abstract> ?o . } }";
 		System.out.println(queryString);
-		
+
 		try {
 			TupleQuery tupleQuery = conn.prepareTupleQuery(QueryLanguage.SPARQL, queryString);
-		    TupleQueryResult result = tupleQuery.evaluate();
-		    while (result.hasNext()) {
-		    	BindingSet bindingSet = result.next();
+			TupleQueryResult result = tupleQuery.evaluate();
+			while (result.hasNext()) {
+				BindingSet bindingSet = result.next();
 
-//		    	Value valueOfP = bindingSet.getValue("p");
-//		    	System.out.println("?p = " + valueOfP.stringValue());
+				// Value valueOfP = bindingSet.getValue("p");
+				// System.out.println("?p = " + valueOfP.stringValue());
 
-		    	Value valueOfO = bindingSet.getValue("g");
-		    	System.out.println("?g = " + valueOfO.stringValue() + "\n");
-		    }
-		    
+				Value valueOfO = bindingSet.getValue("g");
+				System.out.println("?g = " + valueOfO.stringValue() + "\n");
+			}
+
 		} catch (MalformedQueryException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -54,7 +52,7 @@ public class HBaseSailTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		conn.close();
 	}
 
