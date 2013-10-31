537303b107065dc2f4334754fb9c622e556f347d
fundatureanu-sever <fundatureanu.sever@gmail.com>
2013-10-31 18:15:24 +0100

    Added asynchbase to runHBaseSparqlEngine script

--- runHBaseSparqlEngine.sh
+++ runHBaseSparqlEngine.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xmx2048M -cp lib/fuseki-server.jar:target/hbase-0.0.1-SNAPSHOT-jar-with-dependencies.jar org.apache.jena.fuseki.FusekiCmd --config=fuseki-config-hbase.ttl
+java -Xmx2048M -cp lib/fuseki-server.jar:lib/asynchbase-1.5.0-SNAPSHOT.jar:target/hbase-0.0.1-SNAPSHOT-jar-with-dependencies.jar org.apache.jena.fuseki.FusekiCmd --config=fuseki-config-hbase.ttl
