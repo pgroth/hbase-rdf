a70bae144ce3afce79e39903a88699db6caf9f9c
fundatureanu-sever <fundatureanu.sever@gmail.com>
2013-12-04 23:32:35 +0100

    Tweaked planning algorithm to generate only merge blocks with the same join variables

--- src/main/java/nl/vu/jena/sparql/engine/hsp/HSPBlockPlanner.java
+++ src/main/java/nl/vu/jena/sparql/engine/hsp/HSPBlockPlanner.java
@@ -1,7 +1,14 @@
 package nl.vu.jena.sparql.engine.hsp;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import nl.vu.jena.sparql.engine.iterator.QueryIterCartesianProduct;
 import nl.vu.jena.sparql.engine.iterator.QueryIterHashJoin;
@@ -11,6 +18,7 @@ import nl.vu.jena.sparql.engine.joinable.JoinListener;
 import nl.vu.jena.sparql.engine.joinable.Joinable;
 import nl.vu.jena.sparql.engine.joinable.TwoWayJoinable;
 
+import com.hp.hpl.jena.graph.Node;
 import com.hp.hpl.jena.graph.Triple;
 import com.hp.hpl.jena.sparql.core.BasicPattern;
 import com.hp.hpl.jena.sparql.engine.ExecutionContext;
@@ -58,8 +66,10 @@ public class HSPBlockPlanner {
 		
 		int evenSize = listSize%2==1 ? (listSize-1) : listSize;
 		for (int i = 0; i<evenSize; i+=2) {
-			ArrayList<String> commonVars = getCommonVariables((Joinable)mergeJoinBlocks.get(i), (Joinable)mergeJoinBlocks.get(i+1));
-			QueryIter newNode = buildNewNode(mergeJoinBlocks.get(i), mergeJoinBlocks.get(i+1), qCxt, commonVars);
+			QueryIter left = mergeJoinBlocks.get(i);
+			QueryIter right = mergeJoinBlocks.get(i+1);
+			ArrayList<String> commonVars = getCommonVariables((Joinable)left, (Joinable)right);
+			QueryIter newNode = buildNewNode(left, right, qCxt, commonVars);
 			levelUp.add(newNode);
 		}
 		
@@ -109,56 +119,59 @@ public class HSPBlockPlanner {
 
 		ArrayList<QueryIter> mergeJoinBlocks = new ArrayList<QueryIter>();
 		HashSet<WeightedGraphNode> maximumISet = null;
-		HashSet<WeightedGraphNode> finalSet = new HashSet<WeightedGraphNode>();
 		
-		while (pattern.size() > 0) {
-			VariableGraph varGraph = new VariableGraph(pattern);
-
-			ArrayList<HashSet<WeightedGraphNode>> maximumISets = MaximumIndependentSet.computeSets(varGraph);
-
+		if (pattern.size() > 1) {
+			BasicPattern patternCopy = new BasicPattern(pattern);
 			
-			if (maximumISets.size() > 1) {
+			while (patternCopy.size() > 0) {
+				VariableGraph varGraph = new VariableGraph(patternCopy);
+
+				ArrayList<HashSet<WeightedGraphNode>> maximumISets = MaximumIndependentSet.computeSets(varGraph);
+
+				if (maximumISets.size() > 1) {
+					maximumISets = applyHeuristicH1H2(patternCopy, maximumISets, false);
+
+					if (maximumISets.size() > 1) {
+
+						maximumISets = applyHeuristicH1H2(patternCopy, maximumISets, true);
+
+						//TODO apply remaining heuristics
+
+					}
+				}
+
+				maximumISet = maximumISets.get(0);
+				Iterator<WeightedGraphNode> iterator = maximumISet.iterator();
+				if (!iterator.hasNext()) {
+					break;
+				}
+				WeightedGraphNode weightedGraphNode = iterator.next();
+
+				BasicPattern newPattern = buildBasicPatternFromWeightedGraphNode(patternCopy, weightedGraphNode.getId());
+				newPattern = handleCommonVariables(newPattern, weightedGraphNode.getId());
+
+				if (newPattern.size() > 0) {
+					QueryIter basicBlock;
+					if (newPattern.size() > 1) {
+						basicBlock = new QueryIterJoinBlock(new QueryIterNullIterator(qCxt), newPattern, qCxt, currentJoinId++);
+					} 
+					else {
+						basicBlock = new TripleMapper(BindingRoot.create(), newPattern.get(0), qCxt);
+					}
+					mergeJoinBlocks.add(basicBlock);
+				}
 				
-				maximumISets = applyHeuristicH1H2(pattern, maximumISets, false);
-				
-				if (maximumISets.size()>1){
-					
-					maximumISets = applyHeuristicH1H2(pattern, maximumISets, true);
-					
-					//TODO apply remaining heuristics
-					
-				}					
+				patternCopy.getList().removeAll(newPattern.getList());
 			}
-			
-			maximumISet = maximumISets.get(0);		
-			
-			for (WeightedGraphNode weightedGraphNode : maximumISet) {
-				BasicPattern newPattern = buildJoinPatternFromWeightedGraphNode(pattern, weightedGraphNode);
-				
-			}
 
-			
+			//mergeJoinBlocks = buildMergeJoinBlocksFromMaxIndependentSet(pattern, qCxt, finalSet);
 		}
-		
-		mergeJoinBlocks = buildMergeJoinBlocksFromMaxIndependentSet(pattern, qCxt, maximumISet);
-		/*else{
-			mergeJoinBlocks = new ArrayList<QueryIter>();
-		}
-		
-		for (Triple triple : pattern) {
-			QueryIter tp =  new TripleMapper(BindingRoot.create(), triple, qCxt);
+		else{
+			QueryIter tp =  new TripleMapper(BindingRoot.create(), pattern.get(0), qCxt);
 			mergeJoinBlocks.add(tp);
-		}*/
+		}
 		
 		return mergeJoinBlocks;
-	}
-
-	private static BasicPattern buildJoinPatternFromWeightedGraphNode(
-			BasicPattern pattern, WeightedGraphNode weightedGraphNode) {
-		
-		//TODO 
-		
-		return null;
 	}
 
 	/**
@@ -191,7 +204,8 @@ public class HSPBlockPlanner {
 		int count = 0;
 		BasicPattern patternCopy = new BasicPattern(pattern);
 		for (WeightedGraphNode weightedGraphNode : maximumISet) {
-			BasicPattern newPattern = buildBasicPatternFromWeightedGraphNode(patternCopy, weightedGraphNode);
+			BasicPattern newPattern = buildBasicPatternFromWeightedGraphNode(patternCopy, weightedGraphNode.getId());
+			newPattern = handleCommonVariables(newPattern, weightedGraphNode.getId());
 			
 			if (onlyLiterals == false) {
 				count = countConstants(count, newPattern);
@@ -223,11 +237,129 @@ public class HSPBlockPlanner {
 		return count;
 	}
 
-	private static ArrayList<QueryIter> buildMergeJoinBlocksFromMaxIndependentSet(BasicPattern pattern, ExecutionContext qCxt, HashSet<WeightedGraphNode> maximumISet) {
+	private static BasicPattern handleCommonVariables(BasicPattern newPattern, String varNodeName) {
+		//count how many times each variable appears 
+		//if there are triple patterns which don't have any other variables, or which have variables which appear in only 1 tp,
+		//		build a block from these triple patterns
+		//if there are multiple common variables, other than the primary one, 
+		//	choose the triple patterns with the least common variable (appears in the least nr of tp)
+		
+		String specialVar = "__DUMMY";
+		LinkedHashMap<String, Integer> varCounts = new LinkedHashMap<String, Integer>();
+		for (Triple triple : newPattern) {
+			byte updated=0;
+			updated += updateVarCountMap(varCounts, varNodeName, triple.getSubject());
+			updated += updateVarCountMap(varCounts, varNodeName, triple.getPredicate());
+			updated += updateVarCountMap(varCounts, varNodeName, triple.getObject());
+			
+			if (updated == 0){
+				varCounts.put(specialVar, 1);
+			}
+		}
+		
+		List<Map.Entry<String, Integer>> varCountList = new ArrayList<Map.Entry<String, Integer>>(varCounts.entrySet());
+		Collections.sort(varCountList, new Comparator<Map.Entry<String, Integer>>(){
+			@Override
+			public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
+				return (o2.getValue()-o1.getValue());
+			}
+		});
+		
+		if (varCountList.size()==0 || varCountList.get(0).getValue()<=1){
+			//we don't have any other common variables between triple patterns
+			return newPattern;
+		}
+		else {//preserve only the triple patterns with the least common variable			
+			int smallest = varCountList.get(varCountList.size()-1).getValue();
+			updatePattern(newPattern, varNodeName, varCounts, smallest);
+			return newPattern;
+		}
+		
+	}
+
+	private static void updatePattern(BasicPattern newPattern, String varNodeName, LinkedHashMap<String, Integer> varCounts, int smallest) {
+		List<Triple> toDelete = new ArrayList<Triple>();
+		for (Triple t : newPattern) {
+			Node subject = t.getSubject();
+			if (subject.isVariable() && !varNodeName.equals(subject.getName())){
+				if (varCounts.get(subject.getName())>smallest){
+					toDelete.add(t);
+					continue;
+				}
+			}
+			
+			Node predicate = t.getPredicate();
+			if (predicate.isVariable() && !varNodeName.equals(predicate.getName())){
+				if (varCounts.get(predicate.getName())>smallest){
+					toDelete.add(t);
+					continue;
+				}
+			}
+			
+			Node object = t.getObject();
+			if (object.isVariable() && !varNodeName.equals(object.getName())){
+				if (varCounts.get(object.getName())>smallest){
+					toDelete.add(t);
+					continue;
+				}
+			}
+		}
+		
+		newPattern.getList().removeAll(toDelete);
+	}
+	
+	private static byte updateVarCountMap(LinkedHashMap<String, Integer> varCounts, String varNodeName, Node node) {
+		if (node.isVariable()){
+			String nodeName = node.getName();
+			if (!nodeName.equals(varNodeName)){
+				Integer varCount = varCounts.get(nodeName);
+				if (varCount==null){
+					varCounts.put(nodeName, 1);
+				}
+				else{
+					varCounts.put(nodeName, varCount+1);
+				}
+				return 1;
+			}		
+		}
+		return 0;
+	}
+	
+	private static BasicPattern buildBasicPatternFromWeightedGraphNode(BasicPattern pattern, String varNodeName) {
+		BasicPattern newPattern = new BasicPattern();
+		
+		for (Triple triple : pattern) {
+			if (triple.getSubject().isVariable()){
+				if (triple.getSubject().getName().equals(varNodeName)){
+					newPattern.add(triple);
+					continue;
+				}		
+			}
+			
+			if (triple.getPredicate().isVariable()){
+				if (triple.getPredicate().getName().equals(varNodeName)){
+					newPattern.add(triple);
+					continue;
+				}		
+			}
+			
+			if (triple.getObject().isVariable()){
+				if (triple.getObject().getName().equals(varNodeName)){
+					newPattern.add(triple);
+					continue;
+				}		
+			}
+		}
+		
+		return newPattern;
+	}
+
+	/*private static ArrayList<QueryIter> buildMergeJoinBlocksFromMaxIndependentSet(BasicPattern pattern, ExecutionContext qCxt, HashSet<WeightedGraphNode> maximumISet) {
 		ArrayList<QueryIter> mergeJoinBlocks = new ArrayList<QueryIter>(maximumISet.size());
 
 		for (WeightedGraphNode weightedGraphNode : maximumISet) {
-			BasicPattern newPattern = buildBasicPatternFromWeightedGraphNode(pattern, weightedGraphNode);
+			BasicPattern newPattern = buildBasicPatternFromWeightedGraphNode(pattern, weightedGraphNode.getId());
+			newPattern = handleCommonVariables(newPattern, weightedGraphNode.getId());
 			
 			if (newPattern.size() > 0) {
 				QueryIter basicBlock;
@@ -244,34 +376,7 @@ public class HSPBlockPlanner {
 		}
 		
 		return mergeJoinBlocks;
-	}
-
-	private static BasicPattern buildBasicPatternFromWeightedGraphNode(BasicPattern pattern, WeightedGraphNode weightedGraphNode) {
-		BasicPattern newPattern = new BasicPattern();
-		
-		for (Triple triple : pattern) {
-			if (triple.getSubject().isVariable()){
-				if (triple.getSubject().getName().equals(weightedGraphNode.getId())){
-					newPattern.add(triple);
-					continue;
-				}		
-			}
-			
-			if (triple.getPredicate().isVariable()){
-				if (triple.getPredicate().getName().equals(weightedGraphNode.getId())){
-					newPattern.add(triple);
-					continue;
-				}		
-			}
-			
-			if (triple.getObject().isVariable()){
-				if (triple.getObject().getName().equals(weightedGraphNode.getId())){
-					newPattern.add(triple);
-					continue;
-				}		
-			}
-		}
-		return newPattern;
-	}
+	}*/
+	
 
 }
